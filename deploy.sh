ba0bf57566071abdd7396e184c4388f5d5aa56fc
Mirko Gutierrez <mirko.gutierrez@appx.cl>
2026-01-22 10:34:32 -0300

    Configuración de CI/CD para Agendalc y optimización de scripts

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+# 1. Bajar la imagen fresca de la nube
+sudo docker pull mirkogutierrezappx/agendalc:latest
+
+# 2. Limpiar el contenedor anterior
 sudo docker stop agendalc-container 2>/dev/null
 sudo docker rm agendalc-container 2>/dev/null
 
-sudo docker build -t agendalc .
-
+# 3. Correr la nueva versión
 sudo docker run \
-           --restart always \
-           -d -p 8081:8081 \
-           --env-file .env \
-           --network appx \
-	   --add-host=host.docker.internal:host-gateway \
-	   --name agendalc-container agendalc
+    --restart always \
+    -d -p 8081:8081 \
+    --env-file .env \
+    --network appx \
+    --add-host=host.docker.internal:host-gateway \
+    --name agendalc-container \
+    mirkogutierrezappx/agendalc:latest
