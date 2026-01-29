d6dc3da4ac7f808f96441150526296ade639f050
Mirko Gutierrez <mirko.gutierrez@appx.cl>
2026-01-29 15:43:31 -0300

    cambio meotdo controlador que ssuvbe la digitalizacion

--- src/main/java/com/agendalc/agendalc/controllers/DigitalizacionController.java
+++ src/main/java/com/agendalc/agendalc/controllers/DigitalizacionController.java
@@ -29,7 +29,7 @@ public class DigitalizacionController {
     @PreAuthorize("hasRole('FUNC')")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Object> digitalizarDocumentos(
-            @RequestPart("request") DigitalizacionRequest request,
+            @ModelAttribute("request") DigitalizacionRequest request,
             @RequestPart("files") List<MultipartFile> files) {
 
         if (files == null || files.isEmpty()) {
