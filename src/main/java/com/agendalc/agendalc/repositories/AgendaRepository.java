e28a73faf21eb50087c98182db37947516ed3333
Mirko Gutierrez <mirko.gutierrez@appx.cl>
2026-02-23 16:05:09 -0300

    cambia buscqueda ede resumen dairio por agenda y no por cita

--- src/main/java/com/agendalc/agendalc/repositories/AgendaRepository.java
+++ src/main/java/com/agendalc/agendalc/repositories/AgendaRepository.java
@@ -1,14 +1,15 @@
 package com.agendalc.agendalc.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.agendalc.agendalc.entities.Agenda;
+import java.util.List;
+import java.time.LocalDate;
 
 @Repository
 public interface AgendaRepository extends JpaRepository<Agenda, Long> {
-    
 
+    List<Agenda> findByFecha(LocalDate fecha);
 
 }
--- src/main/java/com/agendalc/agendalc/services/ResumenDiarioSeviceImpl.java
+++ src/main/java/com/agendalc/agendalc/services/ResumenDiarioSeviceImpl.java
@@ -2,7 +2,8 @@ package com.agendalc.agendalc.services;
 
 import com.agendalc.agendalc.dto.CitaDto;
 import com.agendalc.agendalc.dto.ResumenDarioResponse;
-import com.agendalc.agendalc.entities.Cita;
+import com.agendalc.agendalc.entities.Agenda;
+import com.agendalc.agendalc.repositories.AgendaRepository;
 import com.agendalc.agendalc.repositories.AsistenciaRepository;
 import com.agendalc.agendalc.repositories.CitaRepository;
 import com.agendalc.agendalc.services.interfaces.ResumenDiarioSevice;
@@ -19,12 +20,14 @@ public class ResumenDiarioSeviceImpl implements ResumenDiarioSevice {
     private final CitaRepository citaRepository;
     private final AsistenciaRepository asistenciaCitaRepository;
     private final CitaMapper citaMapper;
+    private final AgendaRepository agendaRepository;
 
     public ResumenDiarioSeviceImpl(CitaRepository citaRepository, AsistenciaRepository asistenciaCitaRepository,
-            CitaMapper citaMapper) {
+            CitaMapper citaMapper, AgendaRepository agendaRepository) {
         this.citaRepository = citaRepository;
         this.asistenciaCitaRepository = asistenciaCitaRepository;
         this.citaMapper = citaMapper;
+        this.agendaRepository = agendaRepository;
     }
 
     @Override
@@ -34,9 +37,12 @@ public class ResumenDiarioSeviceImpl implements ResumenDiarioSevice {
 
         int asistencias = asistenciaCitaRepository.countByCitaAgendaFecha(fecha);
 
-        List<Cita> citas = citaRepository.findByFechaHoraBetween(fecha.atStartOfDay(), fecha.atTime(23, 59));
+        List<Agenda> agendas = agendaRepository.findByFecha(fecha);
 
-        List<CitaDto> citaDtos = citaMapper.toDtoList(citas);
+        List<CitaDto> citaDtos = agendas.stream()
+                .flatMap(agenda -> agenda.getCitas().stream())
+                .map(citaMapper::toDto)
+                .toList();
 
         return new ResumenDarioResponse(citados, asistencias, citaDtos);
 
