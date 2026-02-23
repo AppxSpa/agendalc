9bd8772650569b1d2948a2f699a841513a80bd05
Mirko Gutierrez <mirko.gutierrez@appx.cl>
2026-02-23 09:22:43 -0300

    ultimo cambios, funcionando

--- src/main/java/com/agendalc/agendalc/services/interfaces/DashboardService.java
+++ src/main/java/com/agendalc/agendalc/services/interfaces/DashboardService.java
@@ -1,7 +1,9 @@
 package com.agendalc.agendalc.services.interfaces;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.agendalc.agendalc.dto.DetalleAgendamientoContribuyente;
 import com.agendalc.agendalc.dto.IndicadoresDiariosDto;
 import com.agendalc.agendalc.dto.IndicadoresDto;
 
@@ -10,5 +12,7 @@ public interface DashboardService {
     IndicadoresDto obtenerIndicadoresDashboard(LocalDate fechaInicio, LocalDate fechaFin);
 
     IndicadoresDiariosDto obtenerIndicadoresDiarios(LocalDate fecha, Long idTipotramite);
+
+    List<DetalleAgendamientoContribuyente> obtenerCitadosDeldia(LocalDate fecha);
 
 }
